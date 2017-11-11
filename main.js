ec7e1ca54c711d5fa1afa374f273ff1692c303b2
Jeremy Potter <git@stormdesign.us>
2017-11-11 14:46:16 -0800

    Add express

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+
+const express = require("express")
+const app = express()
+
+app.get("/", (request, result)=>{
+  result.send("<h1>Hello, World!</h1>")
+})
+
+app.listen(3000, ()=>{
+  console.log("It's live! Go to https://localhost:3000 to view the application.")
+})
